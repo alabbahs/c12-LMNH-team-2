fa771e068bc819a25c3ca5e9a87b77d47e651fe7
Jeremiah <jeremiahbarte@gmail.com>
2024-08-06 10:01:31 +0100

    got the extract pipeline working, it saves a dataframe to a parquet file

--- extract/constants.py
+++ extract/constants.py
@@ -7,6 +7,7 @@ class Constants:
     LMNH_API_ENDPOINT = "https://data-eng-plants-api.herokuapp.com/plants"
     PATH_TO_DATA = "data/"
     NUMBER_OF_PLANTS = 51
+    DATA_FILENAME = "new_data.parquet"
 
     def __setattr__(self, name: str, value: Any) -> None:
         raise AttributeError(f"Can't Reassign Constant {name}")
